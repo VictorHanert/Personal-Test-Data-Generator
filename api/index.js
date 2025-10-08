9a796205b532a4474f530acf7901aa15ec7c5889
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-10-08 17:47:23 +0200

    api - added the main endpoints needed (without any logic so far)

--- api/index.js
+++ api/index.js
@@ -7,6 +7,68 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+// GET /cpr
+app.get("/cpr", (_req, res) => {
+  res.json({ CPR: getCPR() });
+});
+
+// GET /name-gender
+app.get("/name-gender", (_req, res) => {
+  const { firstName, lastName, gender } = getFullNameAndGender();
+  res.json({ firstName, lastName, gender });
+});
+
+// GET /name-gender-dob
+app.get("/name-gender-dob", (_req, res) => {
+  const { firstName, lastName, gender, birthDate } = getFullNameGenderAndBirthDate();
+  res.json({ firstName, lastName, gender, birthDate });
+});
+
+// GET /cpr-name-gender
+app.get("/cpr-name-gender", (_req, res) => {
+  const { firstName, lastName, gender } = getFullNameAndGender();
+  const CPR = getCPR();
+  res.json({ CPR, firstName, lastName, gender });
+});
+
+// GET /cpr-name-gender-dob
+app.get("/cpr-name-gender-dob", (_req, res) => {
+  const { firstName, lastName, gender, birthDate } = getFullNameGenderAndBirthDate();
+  const CPR = getCPR();
+  res.json({ CPR, firstName, lastName, gender, birthDate });
+});
+
+// GET /address
+app.get("/address", async (_req, res) => {
+  try {
+    res.json(await getAddress());
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+// GET /phone
+app.get("/phone", (_req, res) => {
+  res.json({ phoneNumber: getPhoneNumber() });
+});
+
+// GET /person and /person?n=#
+app.get("/person", async (req, res) => {
+  try {
+    const n = req.query.n;
+    if (n) {
+      return res.json(await getFakePersons(n));
+    }
+    return res.json(await getFakePerson());
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
